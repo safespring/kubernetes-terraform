a45e88c87192bdce330eee8f60926921ba851ac2
Jon Ander Novella <novella.uu@gmail.com>
2018-09-05 11:27:01 +0200

    Installed Terraform in box

--- setup.sh
+++ setup.sh
@@ -2,10 +2,15 @@
 
 yum install -y unzip vim python2-pip nc
 
+# install terraform
+mkdir -p ~/bin
+curl -LC - -o /tmp/terraform.zip https://releases.hashicorp.com/terraform/0.11.3/terraform_0.11.3_linux_amd64.zip
+unzip -oq /tmp/terraform.zip -d /usr/local/bin
+
 # install conda
 curl -L -o /tmp/miniconda.sh https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
 chmod +x /tmp/miniconda.sh
 sh /tmp/miniconda.sh -u -b -p /opt/miniconda
 cat >> /home/vagrant/.bashrc << END
-export PATH=/opt/miniconda/bin:$PATH
+export PATH=/usr/local/bin:/opt/miniconda/bin:$PATH
 END
