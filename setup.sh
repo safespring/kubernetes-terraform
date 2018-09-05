83f6dddda5ee2b3ad59901f9aae6581027387277
Jan Ivar Beddari <code@beddari.net>
2018-09-05 11:07:52 +0200

    Some fixes to setup.sh
    With these changes we're now able to repeat "vagrant provision"

--- setup.sh
+++ setup.sh
@@ -3,8 +3,9 @@
 yum install -y unzip vim python2-pip nc
 
 # install conda
-curl -LC - -o /tmp/miniconda.sh https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
+curl -L -o /tmp/miniconda.sh https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
 chmod +x /tmp/miniconda.sh
-sh /tmp/miniconda.sh -b -p /opt/miniconda
+sh /tmp/miniconda.sh -u -b -p /opt/miniconda
 cat >> /home/vagrant/.bashrc << END
 export PATH=/opt/miniconda/bin:$PATH
+END
