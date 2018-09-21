ae573ffa3ddac39310700e1e56650f204f8f4959
Jon Ander Novella <novella.uu@gmail.com>
2018-09-21 09:50:13 +0200

    Box reboot updates and removed unknown services

--- terraform/keys.sh
+++ terraform/keys.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-mkdir -p /home/vagrant/.ssh
-chmod 700 /home/vagrant/.ssh
-ssh-keygen -t rsa -b 4096 -f /home/vagrant/.ssh/id_rsa -q -N ""
-chmod -R 600 /home/vagrant/.ssh/id_rsa
-cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
+PRIVATE_KEY=/home/vagrant/.ssh/id_rsa
+if [ ! -f $PRIVATE_KEY ]; then
+	mkdir -p /home/vagrant/.ssh
+	chmod 700 /home/vagrant/.ssh
+	ssh-keygen -t rsa -b 4096 -f /home/vagrant/.ssh/id_rsa -q -N ""
+	chmod -R 600 /home/vagrant/.ssh/id_rsa
+	cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
+fi
 eval "$(ssh-agent -s)"
 ssh-add /home/vagrant/.ssh/id_rsa
