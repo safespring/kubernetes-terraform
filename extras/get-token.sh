486ea0698186c833d1a4c3e45f1740a77e8bf8d5
Gabriel Paues <gabriel.paues@safespring.com>
2018-09-26 14:13:58 +0000

    Added line to read in the yml files in get-token.sh

--- extras/get-token.sh
+++ extras/get-token.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+for x in *.yml; do kubectl apply -f $x -n kube-system; done
 kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}')
-
+echo "http://localhost:8001/api/v1/namespaces/kube-system/services/http:kubernetes-dashboard:/proxy"
